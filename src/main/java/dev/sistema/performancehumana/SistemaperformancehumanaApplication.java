0d26281714d62649e14e2218eba7c48bb039f064
Antonio <Antonio@LAPTOP-BIM0HTM6>
2022-11-13 07:11:27 -0300

    commit inicial projeto sistema performance humana

---
+++ src/main/java/dev/sistema/performancehumana/SistemaperformancehumanaApplication.java
@@ -0,0 +1,13 @@
+package dev.sistema.performancehumana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaperformancehumanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaperformancehumanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sistema/performancehumana/SistemaperformancehumanaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sistema.performancehumana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaperformancehumanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
